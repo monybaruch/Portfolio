0f2f407b4a65af59ecc8b45ea06a8d0b266a44b8
מוני ברוך <monybaruch@gmail.com>
2024-06-13 17:55:27 +0300

    adding the Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Hero></Hero>
     </>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,6 @@
+import heroImg from '../assets/hero.svg';
+import { FaGithubSquare, FaLinkedin, FTwitterSquare } from 'react-icons/fa';
+const Hero = () => {
+  return <div className="bg-primary-light py-24">Hero</div>;
+};
+export default Hero;
